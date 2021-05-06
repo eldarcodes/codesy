5ee896bddab1255acd29ee8f911cb7b923d79219
mirzabekov00 <prog.eldar@gmail.com>
2021-05-06 22:10:12 +0200

    apollo-express init

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { User } from "./entity/User";
+import * as Express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+const resolvers = {
+  Query: {
+    hello: () => "hi",
+  },
+};
 
-    console.log("Here you can setup and run express/koa/any other framework.");
-  })
-  .catch((error) => console.log(error));
+async function startApolloServer() {
+  const app = Express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  server.applyMiddleware({ app });
+
+  app.listen(4000, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+}
+
+startApolloServer();

557c0bf1c7b58be8ca68a98d3c09171cd2ef8160
mirzabekov00 <prog.eldar@gmail.com>
2021-06-05 19:11:16 +0300

    refactor(server): add session type for mycontext

--- packages/server/src/context.ts
+++ packages/server/src/context.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
+import { Session, SessionData } from "express-session";
 
 export interface MyContext {
-  req: Request;
+  req: Request & {
+    session: Session & Partial<SessionData> & { userId?: string };
+  };
   res: Response;
 }

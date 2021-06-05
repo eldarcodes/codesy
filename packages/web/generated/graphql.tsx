2aa1a8ad087d48af68c4f10f188fecab616ca1fd
mirzabekov00 <prog.eldar@gmail.com>
2021-06-05 20:29:30 +0300

    feat(web): createOffer schema, home fix

--- packages/web/generated/graphql.tsx
+++ packages/web/generated/graphql.tsx
@@ -1,14 +1,10 @@
-import { gql } from "@apollo/client";
-import * as Apollo from "@apollo/client";
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions = {};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -19,193 +15,230 @@ export type Scalars = {
 };
 
 export type CodeReview = {
-  __typename?: "CodeReview";
-  id: Scalars["ID"];
-  numDays?: Maybe<Scalars["Int"]>;
-  codeUrl: Scalars["String"];
-  techTags: Array<Scalars["String"]>;
-  notes: Scalars["String"];
-  ownerId: Scalars["String"];
+  __typename?: 'CodeReview';
+  id: Scalars['ID'];
+  numDays?: Maybe<Scalars['Int']>;
+  codeUrl: Scalars['String'];
+  techTags: Array<Scalars['String']>;
+  notes: Scalars['String'];
+  ownerId: Scalars['String'];
   owner: User;
 };
 
 export type CreateCodeReviewInput = {
-  numDays?: Maybe<Scalars["Int"]>;
-  codeUrl: Scalars["String"];
-  techTags: Array<Scalars["String"]>;
-  notes: Scalars["String"];
+  numDays?: Maybe<Scalars['Int']>;
+  codeUrl: Scalars['String'];
+  techTags: Array<Scalars['String']>;
+  notes: Scalars['String'];
 };
 
 export type CreateCodeReviewResponse = {
-  __typename?: "CreateCodeReviewResponse";
+  __typename?: 'CreateCodeReviewResponse';
   errors?: Maybe<Array<Error>>;
   codeReview?: Maybe<CodeReview>;
 };
 
 export type CreateOfferInput = {
-  userId: Scalars["String"];
-  codeReviewId: Scalars["String"];
+  userId: Scalars['String'];
+  codeReviewId: Scalars['String'];
 };
 
 export type CreateOfferResponse = {
-  __typename?: "CreateOfferResponse";
-  ok: Scalars["Boolean"];
+  __typename?: 'CreateOfferResponse';
+  ok: Scalars['Boolean'];
 };
 
 export type Error = {
-  __typename?: "Error";
-  path: Scalars["String"];
-  message: Scalars["String"];
+  __typename?: 'Error';
+  path: Scalars['String'];
+  message: Scalars['String'];
 };
 
 export type LoginInput = {
-  usernameOrEmail: Scalars["String"];
-  password: Scalars["String"];
+  usernameOrEmail: Scalars['String'];
+  password: Scalars['String'];
 };
 
 export type LoginResponse = {
-  __typename?: "LoginResponse";
+  __typename?: 'LoginResponse';
   errors?: Maybe<Array<Error>>;
   user?: Maybe<User>;
 };
 
 export type Mutation = {
-  __typename?: "Mutation";
+  __typename?: 'Mutation';
   createCodeReview: CreateCodeReviewResponse;
   createOffer: CreateOfferResponse;
   login: LoginResponse;
   register: RegisterResponse;
 };
 
+
 export type MutationCreateCodeReviewArgs = {
   input: CreateCodeReviewInput;
 };
+
 
 export type MutationCreateOfferArgs = {
   input: CreateOfferInput;
 };
 
+
 export type MutationLoginArgs = {
   input: LoginInput;
 };
+
 
 export type MutationRegisterArgs = {
   input: RegisterInput;
 };
 
 export type Query = {
-  __typename?: "Query";
+  __typename?: 'Query';
   listCodeReviews: Array<CodeReview>;
   me?: Maybe<User>;
   users?: Maybe<Array<Maybe<User>>>;
 };
 
 export type RegisterInput = {
-  username: Scalars["String"];
-  email: Scalars["String"];
-  password: Scalars["String"];
+  username: Scalars['String'];
+  email: Scalars['String'];
+  password: Scalars['String'];
 };
 
 export type RegisterResponse = {
-  __typename?: "RegisterResponse";
+  __typename?: 'RegisterResponse';
   errors?: Maybe<Array<Error>>;
 };
 
 export type User = {
-  __typename?: "User";
-  id: Scalars["ID"];
-  username: Scalars["String"];
-  email: Scalars["String"];
+  __typename?: 'User';
+  id: Scalars['ID'];
+  username: Scalars['String'];
+  email: Scalars['String'];
 };
 
-export type CodeReviewInfoFragment = { __typename?: "CodeReview" } & Pick<
-  CodeReview,
-  "id" | "numDays" | "codeUrl" | "techTags" | "notes"
->;
+export type CodeReviewInfoFragment = (
+  { __typename?: 'CodeReview' }
+  & Pick<CodeReview, 'id' | 'numDays' | 'codeUrl' | 'techTags' | 'notes'>
+);
 
 export type CreateCodeReviewMutationVariables = Exact<{
   input: CreateCodeReviewInput;
 }>;
 
-export type CreateCodeReviewMutation = { __typename?: "Mutation" } & {
-  createCodeReview: { __typename?: "CreateCodeReviewResponse" } & {
-    codeReview?: Maybe<{ __typename?: "CodeReview" } & CodeReviewInfoFragment>;
-    errors?: Maybe<
-      Array<{ __typename?: "Error" } & Pick<Error, "path" | "message">>
-    >;
-  };
-};
+
+export type CreateCodeReviewMutation = (
+  { __typename?: 'Mutation' }
+  & { createCodeReview: (
+    { __typename?: 'CreateCodeReviewResponse' }
+    & { codeReview?: Maybe<(
+      { __typename?: 'CodeReview' }
+      & CodeReviewInfoFragment
+    )>, errors?: Maybe<Array<(
+      { __typename?: 'Error' }
+      & Pick<Error, 'path' | 'message'>
+    )>> }
+  ) }
+);
+
+export type CreateOfferMutationVariables = Exact<{
+  input: CreateOfferInput;
+}>;
+
+
+export type CreateOfferMutation = (
+  { __typename?: 'Mutation' }
+  & { createOffer: (
+    { __typename?: 'CreateOfferResponse' }
+    & Pick<CreateOfferResponse, 'ok'>
+  ) }
+);
 
 export type LoginMutationVariables = Exact<{
   input: LoginInput;
 }>;
 
-export type LoginMutation = { __typename?: "Mutation" } & {
-  login: { __typename?: "LoginResponse" } & {
-    user?: Maybe<
-      { __typename?: "User" } & Pick<User, "username" | "email" | "id">
-    >;
-    errors?: Maybe<
-      Array<{ __typename?: "Error" } & Pick<Error, "path" | "message">>
-    >;
-  };
-};
+
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & { login: (
+    { __typename?: 'LoginResponse' }
+    & { user?: Maybe<(
+      { __typename?: 'User' }
+      & Pick<User, 'username' | 'email' | 'id'>
+    )>, errors?: Maybe<Array<(
+      { __typename?: 'Error' }
+      & Pick<Error, 'path' | 'message'>
+    )>> }
+  ) }
+);
 
 export type RegisterMutationVariables = Exact<{
   input: RegisterInput;
 }>;
 
-export type RegisterMutation = { __typename?: "Mutation" } & {
-  register: { __typename?: "RegisterResponse" } & {
-    errors?: Maybe<
-      Array<{ __typename?: "Error" } & Pick<Error, "path" | "message">>
-    >;
-  };
-};
 
-export type ListCodeReviewsQueryVariables = Exact<{ [key: string]: never }>;
+export type RegisterMutation = (
+  { __typename?: 'Mutation' }
+  & { register: (
+    { __typename?: 'RegisterResponse' }
+    & { errors?: Maybe<Array<(
+      { __typename?: 'Error' }
+      & Pick<Error, 'path' | 'message'>
+    )>> }
+  ) }
+);
 
-export type ListCodeReviewsQuery = { __typename?: "Query" } & {
-  listCodeReviews: Array<
-    { __typename?: "CodeReview" } & {
-      owner: { __typename?: "User" } & Pick<User, "id" | "email" | "username">;
-    } & CodeReviewInfoFragment
-  >;
-};
+export type ListCodeReviewsQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type MeQueryVariables = Exact<{ [key: string]: never }>;
 
-export type MeQuery = { __typename?: "Query" } & {
-  me?: Maybe<{ __typename?: "User" } & Pick<User, "id" | "username" | "email">>;
-};
+export type ListCodeReviewsQuery = (
+  { __typename?: 'Query' }
+  & { listCodeReviews: Array<(
+    { __typename?: 'CodeReview' }
+    & { owner: (
+      { __typename?: 'User' }
+      & Pick<User, 'id' | 'email' | 'username'>
+    ) }
+    & CodeReviewInfoFragment
+  )> }
+);
+
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MeQuery = (
+  { __typename?: 'Query' }
+  & { me?: Maybe<(
+    { __typename?: 'User' }
+    & Pick<User, 'id' | 'username' | 'email'>
+  )> }
+);
 
 export const CodeReviewInfoFragmentDoc = gql`
-  fragment CodeReviewInfo on CodeReview {
-    id
-    numDays
-    codeUrl
-    techTags
-    notes
-  }
-`;
+    fragment CodeReviewInfo on CodeReview {
+  id
+  numDays
+  codeUrl
+  techTags
+  notes
+}
+    `;
 export const CreateCodeReviewDocument = gql`
-  mutation CreateCodeReview($input: CreateCodeReviewInput!) {
-    createCodeReview(input: $input) {
-      codeReview {
-        ...CodeReviewInfo
-      }
-      errors {
-        path
-        message
-      }
+    mutation CreateCodeReview($input: CreateCodeReviewInput!) {
+  createCodeReview(input: $input) {
+    codeReview {
+      ...CodeReviewInfo
+    }
+    errors {
+      path
+      message
     }
   }
-  ${CodeReviewInfoFragmentDoc}
-`;
-export type CreateCodeReviewMutationFn = Apollo.MutationFunction<
-  CreateCodeReviewMutation,
-  CreateCodeReviewMutationVariables
->;
+}
+    ${CodeReviewInfoFragmentDoc}`;
+export type CreateCodeReviewMutationFn = Apollo.MutationFunction<CreateCodeReviewMutation, CreateCodeReviewMutationVariables>;
 
 /**
  * __useCreateCodeReviewMutation__
@@ -224,46 +257,62 @@ export type CreateCodeReviewMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useCreateCodeReviewMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    CreateCodeReviewMutation,
-    CreateCodeReviewMutationVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useMutation<
-    CreateCodeReviewMutation,
-    CreateCodeReviewMutationVariables
-  >(CreateCodeReviewDocument, options);
+export function useCreateCodeReviewMutation(baseOptions?: Apollo.MutationHookOptions<CreateCodeReviewMutation, CreateCodeReviewMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateCodeReviewMutation, CreateCodeReviewMutationVariables>(CreateCodeReviewDocument, options);
+      }
+export type CreateCodeReviewMutationHookResult = ReturnType<typeof useCreateCodeReviewMutation>;
+export type CreateCodeReviewMutationResult = Apollo.MutationResult<CreateCodeReviewMutation>;
+export type CreateCodeReviewMutationOptions = Apollo.BaseMutationOptions<CreateCodeReviewMutation, CreateCodeReviewMutationVariables>;
+export const CreateOfferDocument = gql`
+    mutation CreateOffer($input: CreateOfferInput!) {
+  createOffer(input: $input) {
+    ok
+  }
 }
-export type CreateCodeReviewMutationHookResult = ReturnType<
-  typeof useCreateCodeReviewMutation
->;
-export type CreateCodeReviewMutationResult =
-  Apollo.MutationResult<CreateCodeReviewMutation>;
-export type CreateCodeReviewMutationOptions = Apollo.BaseMutationOptions<
-  CreateCodeReviewMutation,
-  CreateCodeReviewMutationVariables
->;
+    `;
+export type CreateOfferMutationFn = Apollo.MutationFunction<CreateOfferMutation, CreateOfferMutationVariables>;
+
+/**
+ * __useCreateOfferMutation__
+ *
+ * To run a mutation, you first call `useCreateOfferMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateOfferMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createOfferMutation, { data, loading, error }] = useCreateOfferMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useCreateOfferMutation(baseOptions?: Apollo.MutationHookOptions<CreateOfferMutation, CreateOfferMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateOfferMutation, CreateOfferMutationVariables>(CreateOfferDocument, options);
+      }
+export type CreateOfferMutationHookResult = ReturnType<typeof useCreateOfferMutation>;
+export type CreateOfferMutationResult = Apollo.MutationResult<CreateOfferMutation>;
+export type CreateOfferMutationOptions = Apollo.BaseMutationOptions<CreateOfferMutation, CreateOfferMutationVariables>;
 export const LoginDocument = gql`
-  mutation Login($input: LoginInput!) {
-    login(input: $input) {
-      user {
-        username
-        email
-        id
-      }
-      errors {
-        path
-        message
-      }
+    mutation Login($input: LoginInput!) {
+  login(input: $input) {
+    user {
+      username
+      email
+      id
+    }
+    errors {
+      path
+      message
     }
   }
-`;
-export type LoginMutationFn = Apollo.MutationFunction<
-  LoginMutation,
-  LoginMutationVariables
->;
+}
+    `;
+export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutationVariables>;
 
 /**
  * __useLoginMutation__
@@ -282,38 +331,24 @@ export type LoginMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useLoginMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    LoginMutation,
-    LoginMutationVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useMutation<LoginMutation, LoginMutationVariables>(
-    LoginDocument,
-    options
-  );
-}
+export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, options);
+      }
 export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
 export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
-export type LoginMutationOptions = Apollo.BaseMutationOptions<
-  LoginMutation,
-  LoginMutationVariables
->;
+export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
 export const RegisterDocument = gql`
-  mutation Register($input: RegisterInput!) {
-    register(input: $input) {
-      errors {
-        path
-        message
-      }
+    mutation Register($input: RegisterInput!) {
+  register(input: $input) {
+    errors {
+      path
+      message
     }
   }
-`;
-export type RegisterMutationFn = Apollo.MutationFunction<
-  RegisterMutation,
-  RegisterMutationVariables
->;
+}
+    `;
+export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, RegisterMutationVariables>;
 
 /**
  * __useRegisterMutation__
@@ -332,37 +367,25 @@ export type RegisterMutationFn = Apollo.MutationFunction<
  *   },
  * });
  */
-export function useRegisterMutation(
-  baseOptions?: Apollo.MutationHookOptions<
-    RegisterMutation,
-    RegisterMutationVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(
-    RegisterDocument,
-    options
-  );
-}
+export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, options);
+      }
 export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
 export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
-export type RegisterMutationOptions = Apollo.BaseMutationOptions<
-  RegisterMutation,
-  RegisterMutationVariables
->;
+export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
 export const ListCodeReviewsDocument = gql`
-  query ListCodeReviews {
-    listCodeReviews {
-      ...CodeReviewInfo
-      owner {
-        id
-        email
-        username
-      }
+    query ListCodeReviews {
+  listCodeReviews {
+    ...CodeReviewInfo
+    owner {
+      id
+      email
+      username
     }
   }
-  ${CodeReviewInfoFragmentDoc}
-`;
+}
+    ${CodeReviewInfoFragmentDoc}`;
 
 /**
  * __useListCodeReviewsQuery__
@@ -379,49 +402,26 @@ export const ListCodeReviewsDocument = gql`
  *   },
  * });
  */
-export function useListCodeReviewsQuery(
-  baseOptions?: Apollo.QueryHookOptions<
-    ListCodeReviewsQuery,
-    ListCodeReviewsQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<ListCodeReviewsQuery, ListCodeReviewsQueryVariables>(
-    ListCodeReviewsDocument,
-    options
-  );
-}
-export function useListCodeReviewsLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<
-    ListCodeReviewsQuery,
-    ListCodeReviewsQueryVariables
-  >
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<
-    ListCodeReviewsQuery,
-    ListCodeReviewsQueryVariables
-  >(ListCodeReviewsDocument, options);
-}
-export type ListCodeReviewsQueryHookResult = ReturnType<
-  typeof useListCodeReviewsQuery
->;
-export type ListCodeReviewsLazyQueryHookResult = ReturnType<
-  typeof useListCodeReviewsLazyQuery
->;
-export type ListCodeReviewsQueryResult = Apollo.QueryResult<
-  ListCodeReviewsQuery,
-  ListCodeReviewsQueryVariables
->;
+export function useListCodeReviewsQuery(baseOptions?: Apollo.QueryHookOptions<ListCodeReviewsQuery, ListCodeReviewsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<ListCodeReviewsQuery, ListCodeReviewsQueryVariables>(ListCodeReviewsDocument, options);
+      }
+export function useListCodeReviewsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<ListCodeReviewsQuery, ListCodeReviewsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<ListCodeReviewsQuery, ListCodeReviewsQueryVariables>(ListCodeReviewsDocument, options);
+        }
+export type ListCodeReviewsQueryHookResult = ReturnType<typeof useListCodeReviewsQuery>;
+export type ListCodeReviewsLazyQueryHookResult = ReturnType<typeof useListCodeReviewsLazyQuery>;
+export type ListCodeReviewsQueryResult = Apollo.QueryResult<ListCodeReviewsQuery, ListCodeReviewsQueryVariables>;
 export const MeDocument = gql`
-  query Me {
-    me {
-      id
-      username
-      email
-    }
+    query Me {
+  me {
+    id
+    username
+    email
   }
-`;
+}
+    `;
 
 /**
  * __useMeQuery__
@@ -438,18 +438,14 @@ export const MeDocument = gql`
  *   },
  * });
  */
-export function useMeQuery(
-  baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options);
-}
-export function useMeLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<MeQuery, MeQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions };
-  return Apollo.useLazyQuery<MeQuery, MeQueryVariables>(MeDocument, options);
-}
+export function useMeQuery(baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+      }
+export function useMeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<MeQuery, MeQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+        }
 export type MeQueryHookResult = ReturnType<typeof useMeQuery>;
 export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>;
 export type MeQueryResult = Apollo.QueryResult<MeQuery, MeQueryVariables>;
--- packages/web/pages/home.tsx
+++ packages/web/pages/home.tsx
@@ -6,6 +6,8 @@ import withApollo from "../lib/withApollo";
 
 interface HomeProps {}
 
+const MAX_NOTES_CHAR_COUNT = 90;
+
 const Home: React.FC<HomeProps> = ({}) => {
   const { data, loading } = useListCodeReviewsQuery();
 
@@ -15,20 +17,19 @@ const Home: React.FC<HomeProps> = ({}) => {
 
   return (
     <Layout>
-      <Grid columns={1} padded>
+      <Grid columns={4} padded>
         {data?.listCodeReviews.map((crr) => (
           <Grid.Column key={crr.id}>
-            {/* <Segment>
-              <div>{crr.notes.slice(0, 150)}</div>
-              <div>{crr.owner.email}</div>
-            </Segment> */}
-            <Card>
+            <Card style={{ height: "100%" }}>
               <Card.Content>
                 <Card.Header>{crr.owner.username} wants a review</Card.Header>
                 <Card.Meta>
                   <span className="date">in {crr.numDays} days</span>
                 </Card.Meta>
-                <Card.Description>{crr.notes.slice(0, 150)}</Card.Description>
+                <Card.Description>
+                  {crr.notes.slice(0, MAX_NOTES_CHAR_COUNT)}
+                  {crr.notes.length > MAX_NOTES_CHAR_COUNT ? "..." : ""}
+                </Card.Description>
               </Card.Content>
               <Card.Content extra>
                 <a href={crr.codeUrl} target="_blank" rel="noopener noreferrer">

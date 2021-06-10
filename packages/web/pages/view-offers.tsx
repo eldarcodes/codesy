e51c47f7afdb2c635c3318a89fd90e4dbc6d3bab
mirzabekov00 <prog.eldar@gmail.com>
2021-06-10 23:10:22 +0300

    style(web): add offers cards

--- packages/web/pages/view-offers.tsx
+++ packages/web/pages/view-offers.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Card } from "semantic-ui-react";
 import Layout from "../components/Layout";
 import { useReceivedOffersQuery } from "../generated/graphql";
 import withApollo from "../lib/withApollo";
@@ -8,6 +9,39 @@ interface ViewOffersProps {}
 const ViewOffers: React.FC<ViewOffersProps> = ({}) => {
   const { data, loading } = useReceivedOffersQuery();
   console.log(data);
-  return <Layout showMenu title="Offers"></Layout>;
+  return (
+    <Layout showMenu title="Offers">
+      <Card.Group itemsPerRow={4}>
+        {data?.receivedOffers.map((offer, index) => (
+          <Card key={index}>
+            <Card.Content>
+              <Card.Header>{offer.sender.username}</Card.Header>
+              <Card.Meta>
+                <a
+                  href={offer.codeReview.codeUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "#4183c4" }}
+                >
+                  Code URL
+                </a>
+              </Card.Meta>
+              <Card.Description>{offer.codeReview.notes}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <Button basic color="green">
+                  Approve
+                </Button>
+                <Button basic color="red">
+                  Decline
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </Layout>
+  );
 };
 export default withApollo({ ssr: true })(ViewOffers);

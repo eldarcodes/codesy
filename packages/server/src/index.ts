f04b7eb03c7492c73f467dd11ff18cd07345518b
mirzabekov00 <prog.eldar@gmail.com>
2021-06-21 21:03:30 +0300

    fix(db): synchronize -> true

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -42,7 +42,7 @@ async function startApolloServer() {
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
         maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
       },
     })

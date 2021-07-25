a193cb141dfc2ebf47cc164371236fc796211a99
mirzabekov00 <prog.eldar@gmail.com>
2021-07-25 15:16:52 +0300

    refactor(global): add storybook components library, connect to web

---
+++ packages/ui/src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: "small" | "medium" | "large";
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button type="button" style={{ backgroundColor }} {...props}>
+      {label}
+    </button>
+  );
+};

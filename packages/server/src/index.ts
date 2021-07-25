d2dad33f0154b9b4b4b68e21b73ce560a94eda87
mirzabekov00 <prog.eldar@gmail.com>
2021-07-25 22:11:18 +0300

    feat(server): add redis session

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,14 +1,22 @@
 import "reflect-metadata";
 import * as Express from "express";
+import * as session from "express-session";
+import * as connectRedis from "connect-redis";
+import * as passport from "passport";
+import { RedisClient } from "redis";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import { Strategy as GitHubStrategy } from "passport-github";
 
 import { createTypeormConnection } from "./createTypeormConnection";
 import { UserResolver } from "./modules/user/UserResolver";
-import * as passport from "passport";
-import { Strategy as GitHubStrategy } from "passport-github2";
+import { User } from "./entity/User";
+import { redis } from "./redis";
 
 require("dotenv-safe").config();
+
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const RedisStore = connectRedis(session);
 
 async function startApolloServer() {
   const app = Express();
@@ -23,6 +31,23 @@ async function startApolloServer() {
     schema,
   });
 
+  const sessionOption: session.SessionOptions = {
+    store: new RedisStore({
+      client: (redis as unknown) as RedisClient,
+    }),
+    name: "qid",
+    secret: SESSION_SECRET || "",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+    },
+  };
+
+  app.use(session(sessionOption));
+
   passport.use(
     new GitHubStrategy(
       {
@@ -30,24 +55,40 @@ async function startApolloServer() {
         clientSecret: process.env.GITHUB_CLIENT_SECRET!,
         callbackURL: "http://localhost:4000/oauth/github",
       },
-      (accessToken: any, refreshToken: any, profile: any, done: any) => {
-        console.log(profile, accessToken, refreshToken);
-        done(null, {});
+      async (accessToken, refreshToken, profile: any, done) => {
+        let user = await User.findOne({
+          where: {
+            githubId: profile.id,
+          },
+        });
+        if (user) {
+        } else {
+          user = await User.create({
+            githubId: profile.id,
+            pictureUrl: profile._json.avatar_url,
+            bio: profile._json.bio,
+          }).save();
+        }
+
+        done(null, {
+          user,
+          accessToken,
+          refreshToken,
+        });
       }
     )
   );
+  app.use(passport.initialize());
 
   app.get("/auth/github", passport.authenticate("github", { session: false }));
 
-  app.get(
-    "/oauth/github",
-    passport.authenticate("github"),
-    function (req, res) {
-      console.log(req.user);
-      // Successful authentication, redirect home.
-      res.redirect("http://localhost:3000/home");
-    }
-  );
+  app.get("/oauth/github", passport.authenticate("github"), (req: any, res) => {
+    req.session.userId = req.user.id;
+    req.session.accessToken = req.user.accessToken;
+    req.session.refreshToken = req.user.refreshToken;
+
+    res.redirect("http://localhost:3000/home");
+  });
 
   server.applyMiddleware({ app });
 

3277a7be5f659905fc98561e8d6d1d4cfbacf1f1
mirzabekov00 <prog.eldar@gmail.com>
2021-07-25 21:19:24 +0300

    feat(server): setup passport-github2 oauth

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,6 +5,10 @@ import { buildSchema } from "type-graphql";
 
 import { createTypeormConnection } from "./createTypeormConnection";
 import { UserResolver } from "./modules/user/UserResolver";
+import * as passport from "passport";
+import { Strategy as GitHubStrategy } from "passport-github2";
+
+require("dotenv-safe").config();
 
 async function startApolloServer() {
   const app = Express();
@@ -18,6 +22,32 @@ async function startApolloServer() {
   const server = new ApolloServer({
     schema,
   });
+
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        callbackURL: "http://localhost:4000/oauth/github",
+      },
+      (accessToken: any, refreshToken: any, profile: any, done: any) => {
+        console.log(profile, accessToken, refreshToken);
+        done(null, {});
+      }
+    )
+  );
+
+  app.get("/auth/github", passport.authenticate("github", { session: false }));
+
+  app.get(
+    "/oauth/github",
+    passport.authenticate("github"),
+    function (req, res) {
+      console.log(req.user);
+      // Successful authentication, redirect home.
+      res.redirect("http://localhost:3000/home");
+    }
+  );
 
   server.applyMiddleware({ app });
 

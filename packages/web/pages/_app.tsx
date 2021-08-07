7fcf260ad64e41dabb02b2d2f3e7a862fd791c9c
mirzabekov00 <prog.eldar@gmail.com>
2021-08-07 16:45:14 +0300

    feat(web): add support for styled-components to nextjs

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,5 +1,35 @@
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/withApollo";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
 function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <>
+      <GlobalStyle />
+
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
+  );
 }
 
 export default MyApp;

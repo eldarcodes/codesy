6c38a2c26b8954f3ca3d0ac6a7f5022a626c4ac5
mirzabekov00 <prog.eldar@gmail.com>
2021-08-08 14:24:19 +0300

    style(ui): update button component

--- packages/ui/src/components/Button/Button.styled.ts
+++ packages/ui/src/components/Button/Button.styled.ts
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 
-const MyButton = styled.button``;
+const MyButton = styled.button`
+  &:hover {
+    color: #40a9ff;
+    border-color: #40a9ff;
+  }
+
+  line-height: 1.5715;
+  position: relative;
+  display: inline-block;
+  font-weight: 400;
+  white-space: nowrap;
+  text-align: center;
+  background-image: none;
+  border: 1px solid transparent;
+  box-shadow: 0 2px #00000004;
+  cursor: pointer;
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  user-select: none;
+  touch-action: manipulation;
+  height: 32px;
+  padding: 4px 15px;
+  font-size: 14px;
+  border-radius: 2px;
+  color: #000000d9;
+  background: #fff;
+  border-color: #d9d9d9;
+`;
 
 export default MyButton;
